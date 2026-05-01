30f8f236834a716581192eff6f76c34d4e598151
Nicolas Dupont <dupontnicolas703@gmail.com>
2026-05-02 03:58:18 +0900

    Moved generation-related files into generation folder

--- generation/house.py
+++ generation/house.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from random import choice
 from typing import Sequence
 
-from gdpc import Editor, Block, Transform
+from gdpc import Block, Editor, Transform
 from gdpc.geometry import placeCuboid, placeCuboidHollow
 from matplotlib import patches
 from mpl_toolkits.axes_grid1.mpl_axes import Axes
@@ -13,6 +13,7 @@ class House:
     """
     Dataclass representing a house that is built in Minecraft
     """
+
     editor: Editor
 
     x: int
@@ -41,16 +42,13 @@ class House:
 
         self.halfWidth = self.width // 2
 
-    def build(self) -> House:
+    def build(self) -> "House":
         """
         Builds the house.
         :return: The house object itself
         """
         with self.editor.pushTransform(
-                Transform(
-                    translation=(self.x, self.y, self.z),
-                    rotation=self.rotation
-                )
+            Transform(translation=(self.x, self.y, self.z), rotation=self.rotation)
         ):
             self.build_shape()
             self.build_door()
@@ -63,10 +61,17 @@ class House:
         :return: None
         """
         # Build walls
-        placeCuboidHollow(self.editor, (0, 0, 0), (self.width, self.height, self.depth), self.wallBlock)
+        placeCuboidHollow(
+            self.editor,
+            (0, 0, 0),
+            (self.width, self.height, self.depth),
+            self.wallBlock,
+        )
 
         # Place blocks below to fit terrain
-        placeCuboid(self.editor, (0, 0, 0), (self.width, -5, self.depth), self.floorPalette)
+        placeCuboid(
+            self.editor, (0, 0, 0), (self.width, -5, self.depth), self.floorPalette
+        )
 
     def build_door(self) -> None:
         """
@@ -79,7 +84,12 @@ class House:
         self.editor.placeBlock((doorPosition, 1, 0), doorBlock)
 
         # Clear some space in front of the door
-        placeCuboid(self.editor, (doorPosition - 1, 1, -1), (doorPosition + 1, 3, -1), Block("air"))
+        placeCuboid(
+            self.editor,
+            (doorPosition - 1, 1, -1),
+            (doorPosition + 1, 3, -1),
+            Block("air"),
+        )
 
     def build_roof(self) -> None:
         """
@@ -92,16 +102,28 @@ class House:
             yy = self.height + self.halfWidth - dx
 
             # Fill empty space in roof
-            placeCuboid(self.editor, (self.halfWidth - dx, yy, 0), (self.halfWidth + dx, yy, self.depth),
-                        self.wallBlock)
+            placeCuboid(
+                self.editor,
+                (self.halfWidth - dx, yy, 0),
+                (self.halfWidth + dx, yy, self.depth),
+                self.wallBlock,
+            )
 
             # Build row of stairs blocks
             leftBlock = Block(roofStairs.id, {"facing": "east"})
             rightBlock = Block(roofStairs.id, {"facing": "west"})
-            placeCuboid(self.editor, (self.halfWidth - dx, yy, -1), (self.halfWidth - dx, yy, self.depth + 1),
-                        leftBlock)
-            placeCuboid(self.editor, (self.halfWidth + dx, yy, -1), (self.halfWidth + dx, yy, self.depth + 1),
-                        rightBlock)
+            placeCuboid(
+                self.editor,
+                (self.halfWidth - dx, yy, -1),
+                (self.halfWidth - dx, yy, self.depth + 1),
+                leftBlock,
+            )
+            placeCuboid(
+                self.editor,
+                (self.halfWidth + dx, yy, -1),
+                (self.halfWidth + dx, yy, self.depth + 1),
+                rightBlock,
+            )
 
             # Add upside-down accent blocks
             leftBlock = Block(roofStairs.id, {"facing": "west", "half": "top"})
@@ -112,9 +134,14 @@ class House:
 
         # build the top row of the roof
         yy = self.height + self.halfWidth - 1
-        placeCuboid(self.editor, (self.halfWidth, yy, -1), (self.halfWidth, yy, self.depth + 1), roofBlock)
+        placeCuboid(
+            self.editor,
+            (self.halfWidth, yy, -1),
+            (self.halfWidth, yy, self.depth + 1),
+            roofBlock,
+        )
 
-    def plot(self, ax: Axes) -> House:
+    def plot(self, ax: Axes) -> "House":
         """
         Plots the house footprint in a graphic
         :param ax: The axe to plot the house in to
@@ -129,14 +156,14 @@ class House:
         rect = patches.Rectangle(
             (
                 self.x + (0 if self.rotation in [0, 3] else 3),
-                self.z + (0 if self.rotation in [0, 1] else 3)
+                self.z + (0 if self.rotation in [0, 1] else 3),
             ),
             self.width + 3,
             self.depth + 3,
-            edgecolor='red',
+            edgecolor="red",
             fill=False,
             alpha=0.6,
-            angle=self.rotation * 90
+            angle=self.rotation * 90,
         )
 
         ax.add_patch(rect)
